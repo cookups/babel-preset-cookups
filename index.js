6411182c4e53b641689f9634d254574ea3343f69
Sabbir Ahmed <sabbiirr@gmail.com>
2018-04-02 17:56:23 +0600

    fix: preset defult errors

--- index.js
+++ index.js
@@ -19,25 +19,32 @@ module.exports = function(api, options) {
   }
 
   var isFlowEnabled = validateBoolOption('flow', options.flow, true);
-  var isReactNative = validateBoolOption('react-native', options.reactNative, true);
+  var isReactNative = validateBoolOption('react-native', options.reactNative, false);
   var isReactNativeWeb = validateBoolOption('react-native-web', options.reactNativeWeb, false);
   var isNode = validateBoolOption('node', options.node, false);
+  var isDefault = validateBoolOption('default', (!isReactNativeWeb && !isReactNativeWeb && !isNode), true)
+
+  var presets = [
+    isDefault && require('babel-preset-babel-preset-es2015'),
+    isDefault && require('babel-preset-babel-preset-es2017'),
+    isFlowEnabled && require('babel-preset-flow').default,
+    isReactNative && require('babel-preset-react-native'),
+    (isNode && (!isReactNative || !isReactNativeWeb))  && [require('babel-preset-env'), {
+      targets: {
+        node: 'current'
+      }
+    }],
+  ];
+
+  var plugins = [
+    require('babel-plugin-syntax-dynamic-import').default,
+    require('babel-plugin-transform-decorators-legacy').default,
+    isReactNativeWeb && require('babel-plugin-react-native-web')
+  ];
 
 
   return {
-    presets: [
-      isReactNative && [require('babel-preset-react-native')],
-      (isNode && !(isReactNative)) && [require('babel-preset-env'), {
-        targets: {
-          node: 'current'
-        }
-      }],
-      isFlowEnabled && [require('babel-preset-flow').default]
-    ].filter(Boolean),
-    plugins: [
-      require('babel-plugin-syntax-dynamic-import').default,
-      require('babel-plugin-transform-decorators-legacy').default,
-      isReactNativeWeb && require('babel-plugin-react-native-web')
-    ].filter(Boolean)
+    presets: presets.filter(Boolean),
+    plugins: plugins.filter(Boolean)
   };
 };
